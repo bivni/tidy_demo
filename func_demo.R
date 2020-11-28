04c4aac8a810f6056f688657585c1a71573ba262
Ilya Kochergin .econ <koch@econ.msu.ru>
2020-11-28 12:42:42 +0300

    func_demo.R move purrr example to purrr1.R

--- func_demo.R
+++ func_demo.R
@@ -57,33 +57,6 @@ lapply(list1, get_city_par, "pop")
 pop_vec <-  sapply(list1, get_pop)
 pop_vec
 
-library(purrr)
 
-(pop_list <- map(list1,get_pop) )
-map_dbl(list1,get_pop)
-map_chr(list1,get_pop)
 
-## side effect  and local and global variables
 
-glob_cnt=0
-
-print_cnt_wrong <- function(increment=1) {
-  print(glob_cnt)
-  glob_cnt<-glob_cnt+increment # local  
-  print(glob_cnt)
-} 
-
-print_cnt_wrong()
-print_cnt_wrong()
-print_cnt_wrong(20)
-print_cnt_wrong()
-
-print_cnt <- function(increment=1) {
-  glob_cnt <<- glob_cnt+increment # global assignment  
-  print(glob_cnt)
-} 
-glob_cnt=0
-print_cnt()
-print_cnt()
-print_cnt(20)
-print_cnt()
