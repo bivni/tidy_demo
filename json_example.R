999505f8b17c23764f3fc9e82f7b7300d51bcb23
Ilya Kochergin .econ <koch@econ.msu.ru>
2020-11-29 16:55:49 +0300

    example solved

--- json_example.R
+++ json_example.R
@@ -7,7 +7,9 @@ weathers_list <- json_contents$list
 map_chr( weathers_list, "dt_txt")
 map_chr( weathers_list, function (item)  item[["dt_txt"]])
 map_chr( weathers_list, ~ .x[["dt_txt"]])
+
 map_dbl( weathers_list, c("main","temp"))
+
 
 prognoz_temp <- tibble(
   time = ymd_hms(map_chr( weathers_list, "dt_txt")),
@@ -19,3 +21,11 @@ prognoz_temp
 # Задача 
 # добавить стобцы wind_deg и wind_speed 
 #
+prognoz_temp <- tibble(
+  time = ymd_hms(map_chr( weathers_list, "dt_txt")),
+  temp = map_dbl( weathers_list, c("main","temp")),
+  wind_deg = map_dbl( weathers_list,c("wind","deg") ),
+  wind_speed = map_dbl( weathers_list,c("wind","speed") )
+
+)
+
