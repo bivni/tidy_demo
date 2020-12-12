33ab8534da55f03e3e50c8ea9022317dbd4c672f
Ilya Kochergin .econ <koch@econ.msu.ru>
2020-12-12 14:07:14 +0300

    comment  jupyter_conver.Rmd

--- jupyter_convert.R
+++ jupyter_convert.R
@@ -1,6 +1,6 @@
 library(markdown)
 nb_file="r-simple-flowcontrol.ipynb"
-# xfun::file_string(nb_file)
+# xfun::file_string(nb_file)   # show content of file 
 nb_rmd = rmarkdown:::convert_ipynb(nb_file)
 print(nb_rmd)
 
